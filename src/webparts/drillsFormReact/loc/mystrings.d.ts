8d56c3b2da04e230058bd004a95ed2aed177642d
Duygu Kayabasi <kayabasiduygu@gmail.com>
2022-06-30 22:54:00 +0300

    first commit

---
+++ src/webparts/drillsFormReact/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IDrillsFormReactWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'DrillsFormReactWebPartStrings' {
+  const strings: IDrillsFormReactWebPartStrings;
+  export = strings;
+}
